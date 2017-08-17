cc2e3acabefcb511a4b6012a85b7ace020c7974c
vidhitad <bhakti_phatak@yahoo.com>
2017-08-17 15:37:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/hash/:input', function(req,res){
      //   }
     //});
     
-});
+//});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
