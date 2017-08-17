8ea712b26b7e66bae7414767d5831ada05394a43
vidhitad <bhakti_phatak@yahoo.com>
2017-08-17 15:40:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 function hash(input, salt){
     //how to create hash
-    var hashed = crypto.pbkdf2(input,salt, 10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt, 10000,512,'sha512');
     return hashed.toString('hex');
 }
 
